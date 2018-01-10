0e61276dee443728a60930315b8e077f6da5d082
wey46 <32597729+wey46@users.noreply.github.com>
2018-01-10 10:53:10 -0500

    Add files via upload

---
+++ src/EarthQuakeClient2.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import edu.duke.*;
+
+public class EarthQuakeClient2 {
+    public EarthQuakeClient2() {
+        // TODO Auto-generated constructor stub
+    }
+    public static void main(String[] args){
+        EarthQuakeParser parser = new EarthQuakeParser(); 
+        
+        //1.modify input source here(local, or URL from "earthquake.usgs.gov/earthquakes/feed" )
+        
+        //String source = "data/nov20quakedata.atom";
+        String source = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.atom";
+        
+        ArrayList<QuakeEntry> list  = parser.read(source);         
+        System.out.println("read data for "+list.size()+" quakes");
+        MatchAllFilter maf = new MatchAllFilter();
+        
+        //2. modify/disable filtering creterias here
+        maf.addFilter(new DepthFilter(-30000.0, -180000.0));
+        maf.addFilter(new MagnitudeFilter(0.0, 5.0));
+        maf.addFilter(new DistanceFilter(new Location(55.7308, 9.1153), 3000000.0 ));
+        maf.addFilter(new PhraseFilter("any", "e"));
+        
+        ArrayList<QuakeEntry> answer = filter(list, maf);
+        for (QuakeEntry qe: answer) { 
+            System.out.println(qe);
+        } 
+        System.out.println("There are " + answer.size() + " quakes fulfilling the criteria");
+        System.out.println("Filters used are: " + maf.getName());
+        
+        //3.create csv file if you want to save the result...
+
+    }
+    public static ArrayList<QuakeEntry> filter(ArrayList<QuakeEntry> quakeData, Filter f) { 
+        ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
+        for(QuakeEntry qe : quakeData) { 
+            if (f.satisfies(qe)) { 
+                answer.add(qe); 
+            } 
+        } 
+        
+        return answer;
+    } 
+
+    public void quakesWithFilter() { 
+        EarthQuakeParser parser = new EarthQuakeParser(); 
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);         
+        System.out.println("read data for "+list.size()+" quakes");
+        //Filter f1 = new MagnitudeFilter(4.0, 5.0); 
+        //Filter f2 = new DepthFilter(-12000.0, -35000.0);
+        Location tokyolc = new Location(35.42, 139.43);
+        Filter f1 = new DistanceFilter(tokyolc, 10000000);
+        Filter f2 = new PhraseFilter("end", "Japan");
+        ArrayList<QuakeEntry> fst = filter(list, f1);
+        ArrayList<QuakeEntry> scnd = filter(fst, f2); 
+        for (QuakeEntry qe: scnd) { 
+            System.out.println(qe);
+        } 
+    }
+    public void testMatchAllFilter(){
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);         
+        System.out.println("read data for "+list.size()+" quakes");
+        MatchAllFilter maf = new MatchAllFilter();
+        //maf.addFilter(new DepthFilter(-30000.0, -180000.0));
+        maf.addFilter(new MagnitudeFilter(0.0, 5.0));
+        maf.addFilter(new DistanceFilter(new Location(55.7308, 9.1153), 3000000.0 ));
+        maf.addFilter(new PhraseFilter("any", "e"));
+        ArrayList<QuakeEntry> answer = filter(list, maf);
+        for (QuakeEntry qe: answer) { 
+            System.out.println(qe);
+        } 
+        System.out.println("There are " + answer.size() + " quakes fulfilling the criteria");
+        System.out.println("Filters used are: " + maf.getName());
+    }
+
+    public void createCSV() {
+        EarthQuakeParser parser = new EarthQuakeParser();
+        //String source = "../data/nov20quakedata.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        dumpCSV(list);
+        System.out.println("# quakes read: "+list.size());
+    }
+
+    private void dumpCSV(ArrayList<QuakeEntry> list) {
+        System.out.println("Latitude,Longitude,Magnitude,Info");
+        for(QuakeEntry qe : list){
+            System.out.printf("%4.2f,%4.2f,%4.2f,%s\n",
+                qe.getLocation().getLatitude(),
+                qe.getLocation().getLongitude(),
+                qe.getMagnitude(),
+                qe.getInfo());
+        }
+    }
+
+}
